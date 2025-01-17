6b3102d0b263e5cece39187c099ed2f960267786
VK Goswami <vineshgoswami45@gmail.com>
2025-01-17 23:30:06 +0530

    add main function for executing of queries that uer give as input

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def input_command():
             print("Interpreting....")
             query = r.recognize_google(audio, language="en-in")
             print(f"user said: {query}")
-        except Exceptionn as e:
-            print("I did not get that what you are saying, can you say it again? ")
+        except Exception as e:
+            say("I did not get that what you are saying, can you say it again? ")
             return "none"
         return query
 
@@ -23,3 +23,16 @@ def say(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+
+
+def main():
+    say("hey vinesh how are you? , I am you personal assistant ")
+    while True:
+        print("Listening...")
+        query = input_command()
+        if query == 'none':
+            continue
+
+
+if __name__ == "__main__":
+    main()
